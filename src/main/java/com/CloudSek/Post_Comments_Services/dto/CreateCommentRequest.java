c99a5edc9c15c090c16111a204c4488ee9f24398
Soumya Ranjan Mishra <soumyarmishra065@gmail.com>
2025-07-12 16:21:51 +0530

    added and fixing dto classes

--- src/main/java/com/CloudSek/Post_Comments_Services/dto/CreateCommentRequest.java
+++ src/main/java/com/CloudSek/Post_Comments_Services/dto/CreateCommentRequest.java
@@ -1,4 +1,25 @@
 package com.CloudSek.Post_Comments_Services.dto;
 
 public class CreateCommentRequest {
+    @NotBlank(message = "Content is required")
+    private String content;
+
+    @NotBlank(message = "Author is required")
+    @Size(max = 50, message = "Author name must not exceed 50 characters")
+    private String author;
+
+    // Constructors
+    public CreateCommentRequest() {}
+
+    public CreateCommentRequest(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    // Getters and Setters
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 }
--- src/main/java/com/CloudSek/Post_Comments_Services/dto/CreatePostRequest.java
+++ src/main/java/com/CloudSek/Post_Comments_Services/dto/CreatePostRequest.java
@@ -1,4 +1,33 @@
 package com.CloudSek.Post_Comments_Services.dto;
 
 public class CreatePostRequest {
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title must not exceed 100 characters")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    private String content;
+
+    @NotBlank(message = "Author is required")
+    @Size(max = 50, message = "Author name must not exceed 50 characters")
+    private String author;
+
+    // Constructors
+    public CreatePostRequest() {}
+
+    public CreatePostRequest(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 }
